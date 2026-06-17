17dea44d634e2f81cfe004715040acbca45e254f
Samranvirk44 <samranvirk44@gmail.com>
2026-06-17 11:02:55 -0700

    Allow crawlers in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,16 +1,11 @@
 import { MetadataRoute } from 'next'
-import { SITE_URL } from '@/lib/seo'
 
 export default function robots(): MetadataRoute.Robots {
-  const site = SITE_URL.replace(/\/$/, '')
-
   return {
     rules:{
       userAgent:'*',
       allow:'/',
-      disallow:['/admin','/admin/','/dashboard','/dashboard/','/login','/api/','/test'],
     },
-    sitemap:`${site}/sitemap.xml`,
-    host:new URL(site).host,
+    sitemap:'https://vehiqal.com/sitemap.xml',
   }
 }
